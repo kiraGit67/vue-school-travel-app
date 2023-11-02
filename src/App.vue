9ea3c920f9f59bc22f346445879d24483e001f5c
kiraGit67 <kirsten@sais.de>
2023-11-02 16:46:03 +0100

    adds router-link to /panama and to PanamaView.vue

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <router-link to="/brazil">Brazil</router-link>
     <router-link to="/hawaii">Hawaii</router-link>
     <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="panama">Panama</router-link>
   </div>
   <div class="container">
     <router-view></router-view>
