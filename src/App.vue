245f6c6d2a5f527a9ba6ac1e0fc8471cb91d9e35
kiraGit67 <kirsten@sais.de>
2023-11-02 16:33:56 +0100

    adds router-link to BrazilView.vue, wraps div#nav around router-link elements

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
+  <div id="nav">
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
-  </nav>
+    <router-link to="/brazil">Brazil</router-link>
+  </div>
   <div class="container">
     <router-view></router-view>
   </div>
