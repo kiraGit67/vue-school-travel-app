c9ee84471fcd8e3134f46bee269cd33a7bb85533
kiraGit67 <kirsten@sais.de>
2023-11-02 14:48:19 +0100

    adds div.container

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view />
+  <div class="container">
+    <router-view></router-view>
+  </div>
 </template>
