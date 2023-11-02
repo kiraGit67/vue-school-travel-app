25423851d06a42e9861200c27433bab29b7b57e3
kiraGit67 <kirsten@sais.de>
2023-11-02 17:02:49 +0100

    adds data() function into script, adds router-link v-for each destination

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <router-link
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="destination.slug"
+      >
+        <h2>{{ destination.name }}</h2>
+        <img :src="`/images/${destination.image}`" :alt="destination.name" />
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import sourceData from "@/data.json";
+
 export default {
-  name: "HomeView",
-  components: {},
+  data() {
+    return {
+      destinations: sourceData.destinations,
+    };
+  },
 };
 </script>
