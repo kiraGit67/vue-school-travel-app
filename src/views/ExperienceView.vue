134278a35b0d93324594e8dca6ce4dfa28e72be4
kiraGit67 <kirsten@sais.de>
2023-11-04 19:14:40 +0100

    adds src/views/ExperienceView.vue

---
+++ src/views/ExperienceView.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="experience">
+    <h1>{{ experience.name }}</h1>
+    <div class="destination-details">
+      <img
+        :src="`/images/${experience.image}`"
+        :alt="experience.name"
+        :title="experience.name"
+      />
+      <p>{{ experience.description }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    destination() {
+      return sourceData.destinations.find(
+        (destination) => destination.id === this.id
+      );
+    },
+    experience() {
+      return this.destination.experiences.find(
+        (experience) => experience.slug === this.slug
+      );
+    },
+  },
+};
+</script>
