2a0168dec53fa37560d7a32d2aed95981e90e9eb
kiraGit67 <kirsten@sais.de>
2023-11-04 19:41:01 +0100

    adds src/components/ExperienceCard.vue

---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <img
+      :src="`/images/${experience.image}`"
+      :alt="experience.name"
+      :title="experience.name"
+    />
+    <div class="card__text">{{ experience.name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    experience: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
