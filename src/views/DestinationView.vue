9a47bf54a1aca42b6e87607717829ccc12d54e59
kiraGit67 <kirsten@sais.de>
2023-11-27 13:55:28 +0100

    embeds DestinationView sections in a div element

--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -1,32 +1,34 @@
 <template>
-  <section class="destination">
-    <h1>{{ destination.name }}</h1>
-    <go-back />
-    <div class="destination-details">
-      <img
-        :src="`/images/${destination.image}`"
-        :alt="destination.name"
-        :title="destination.name"
-      />
-      <p>{{ destination.description }}</p>
-    </div>
-  </section>
-  <section class="experiences">
-    <h2>Top Experiences in {{ destination.name }}</h2>
-    <div class="cards">
-      <router-link
-        v-for="experience in destination.experiences"
-        :key="experience.slug"
-        :to="{
-          name: 'experience.view',
-          params: { experienceSlug: experience.slug },
-        }"
-      >
-        <experience-card :experience="experience" />
-      </router-link>
-    </div>
-    <router-view />
-  </section>
+  <div>
+    <section class="destination">
+      <h1>{{ destination.name }}</h1>
+      <go-back />
+      <div class="destination-details">
+        <img
+          :src="`/images/${destination.image}`"
+          :alt="destination.name"
+          :title="destination.name"
+        />
+        <p>{{ destination.description }}</p>
+      </div>
+    </section>
+    <section class="experiences">
+      <h2>Top Experiences in {{ destination.name }}</h2>
+      <div class="cards">
+        <router-link
+          v-for="experience in destination.experiences"
+          :key="experience.slug"
+          :to="{
+            name: 'experience.view',
+            params: { experienceSlug: experience.slug },
+          }"
+        >
+          <experience-card :experience="experience" />
+        </router-link>
+      </div>
+      <router-view />
+    </section>
+  </div>
 </template>
 
 <script>
