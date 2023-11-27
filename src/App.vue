1ff764c3f5c0e8f4d066091ec9595e9aa4b68012
kiraGit67 <kirsten@sais.de>
2023-11-27 14:02:50 +0100

    changes transition name from slide to moveUp

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <the-navigation />
   <div class="container">
     <router-view v-slot="{ Component }">
-      <transition name="slide" mode="out-in">
+      <transition name="moveUp">
         <component :is="Component" :key="$route.path"></component>
       </transition>
     </router-view>
@@ -20,14 +20,32 @@ export default {
 </script>
 
 <style lang="css">
-.slide-enter-active,
-.slide-leave-active {
-  transition: opacity 1s, transform 1s;
+.moveUp-enter-active {
+  animation: fadeIn 1s ease-in;
 }
 
-.slide-enter-from,
-.slide-leave-to {
-  opacity: 0;
-  transform: translateX(-30%);
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.moveUp-leave-active {
+  animation: moveUp 0.3s ease-in;
+}
+
+@keyframes moveUp {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-400px);
+  }
 }
 </style>
