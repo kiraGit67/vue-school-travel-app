41a0f6c609133be28ae5c93e28ba903a4eefd7c5
kiraGit67 <kirsten@sais.de>
2023-11-27 16:53:54 +0100

    adds new route to NotFoundView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+//import NotFoundView from "@/views/NotFoundView";
 
 const routes = [
   {
@@ -34,6 +35,11 @@ const routes = [
         }),
       },
     ],
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: () => import("@/views/NotFoundView.vue"),
   },
 ];
 
