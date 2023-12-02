8357a372bdc368a103b853c3cdc5ba0f3055494a
kiraGit67 <kirsten@sais.de>
2023-12-02 20:04:34 +0100

    adds script with data() and methods

--- src/views/ProtectedView.vue
+++ src/views/ProtectedView.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <h1>Protected</h1>
+    <h1>Greetings, {{ username }}</h1>
+    <button class="btn" @click="logout">Logout</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: window.user,
+    };
+  },
+  methods: {
+    logout() {
+      window.user = null;
+      this.$router.push({ name: "home" });
+    },
+  },
+};
+</script>
