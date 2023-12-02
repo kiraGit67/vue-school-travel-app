74a9880aeb63559fc8720dd133885b76897b0212
kiraGit67 <kirsten@sais.de>
2023-12-02 19:33:15 +0100

    adds src/views/LoginView.vue

---
+++ src/views/LoginView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login">
+    <form class="form" @submit.prevent="login">
+      <h1>Login</h1>
+      <label for="username">Username</label>
+      <input v-model="username" type="text" id="username" name="username" />
+      <label for="password">Password</label>
+      <input v-model="password" type="password" id="password" name="password" />
+      <button class="btn">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      window.user = this.username;
+      this.$router.push({ name: "protected" });
+    },
+  },
+};
+</script>
