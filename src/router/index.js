ca4e0181d5cb1c6c696b4bbf2ae34ea9f1399409
kiraGit67 <kirsten@sais.de>
2023-12-02 18:09:39 +0100

    adds new route to AboutView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ const routes = [
     component: HomeView,
   },
   {
+    path: "/about",
+    name: "about",
+    component: () => import("@/views/AboutView.vue"),
+  },
+  {
     path: "/destination/:id/:slug",
     name: "destination.view",
     // route level code-splitting
