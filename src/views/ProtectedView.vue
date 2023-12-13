9ad6e689bb027122820bdae82c6c35a4fc7a984f
kiraGit67 <kirsten@sais.de>
2023-12-13 10:36:39 +0100

    adds hash: "#logout" to logout() method

--- src/views/ProtectedView.vue
+++ src/views/ProtectedView.vue
@@ -18,7 +18,7 @@ export default {
   methods: {
     logout() {
       window.user = null;
-      this.$router.push({ name: "home" });
+      this.$router.push({ name: "home", hash: "#logout" });
     },
   },
 };
