239ccfaf4f801a8c955368bc6650715304b6f708
kiraGit67 <kirsten@sais.de>
2023-12-14 12:08:04 +0100

    imports isNavigationFailure, NavigationFailureType from vue-router to detect navigation failures

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>All Destinations</h1>
+    <p><button @click="triggerRouterError">Trigger Router Error</button></p>
     <div class="destinations">
       <router-link
         v-for="destination in destinations"
@@ -21,12 +22,28 @@
 // @ is an alias to /src
 
 import sourceData from "@/data.json";
+import { isNavigationFailure, NavigationFailureType } from "vue-router";
 
 export default {
   data() {
     return {
       destinations: sourceData.destinations,
     };
+  },
+  methods: {
+    async triggerRouterError() {
+      const navigationFailure = await this.$router.push("/");
+
+      if (
+        isNavigationFailure(navigationFailure, NavigationFailureType.duplicated)
+      ) {
+        //false was returned inside of a navigation guard to the navigation
+        console.log(navigationFailure.to);
+        console.log(navigationFailure.from);
+      } else {
+        //all is allright
+      }
+    },
   },
 };
 </script>
