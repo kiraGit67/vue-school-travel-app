6ad5abde562357abcb7ebbee4e2d8a02505a0397
kiraGit67 <kirsten@sais.de>
2023-12-14 17:48:44 +0100

    adds variables for inputType and buttonText and togglePassword method

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,10 +10,14 @@
         name="username"
         class="input"
       />
-      <label for="password">Password</label>
+      <label for="password"
+        >Password&nbsp;<button @click="togglePassword">
+          {{ buttonText }}
+        </button></label
+      >
       <input
         v-model="password"
-        type="password"
+        :type="inputType"
         id="password"
         name="password"
         class="input"
@@ -30,14 +34,29 @@ export default {
   setup() {
     const username = ref("");
     const password = ref("");
+    let inputType = ref("password");
+    let buttonText = ref("Show Password");
     const router = useRouter();
     const route = useRoute();
+
+    const togglePassword = () => {
+      if (inputType.value === "password") {
+        inputType.value = "text";
+        buttonText.value = "Hide Password";
+      } else {
+        inputType.value = "password";
+        buttonText.value = "Show Password";
+      }
+    };
+
     const login = () => {
       window.user = username.value;
+      window.password = password.value;
       const redirectPath = route.query.redirect || "/protected";
       router.push(redirectPath);
     };
-    return { username, password, login };
+
+    return { username, password, inputType, buttonText, togglePassword, login };
   },
   /*
   data() {
