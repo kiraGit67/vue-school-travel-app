5dee60e0a1aaa28775afdce3dbfb67d90c32d79f
kiraGit67 <kirsten@sais.de>
2023-12-14 14:15:28 +0100

    adds script to get username

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <h1>Your Orders</h1>
+    <h1>{{ username }}‘s Orders</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: window.user,
+    };
+  },
+  methods: {
+    logout() {
+      window.user = null;
+      this.$router.push({ name: "home", query: { logout: null } });
+    },
+  },
+};
+</script>
