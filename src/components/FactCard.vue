e6cd38fea236e46842ee91606af701639f265342
kiraGit67 <kirsten@sais.de>
2023-12-17 16:12:15 +0100

    adds src/components/FactCard.vue

---
+++ src/components/FactCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <article class="fact">
+    <img
+      :src="fact.img"
+      :alt="fact.count + ' ' + fact.title"
+      :title="fact.count + ' ' + fact.title"
+    />
+    <div class="text">
+      <h2>{{ fact.count + " " + fact.title }}</h2>
+      <p>{{ fact.description }}</p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    fact: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fact {
+  display: flex;
+  gap: 1rem;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0.5rem;
+  background-color: white;
+  border: 1px solid #686dfa;
+  border-radius: 0.5rem;
+}
+
+.fact h2 {
+  color: #686dfa;
+}
+</style>
