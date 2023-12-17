98b638c2497084724020371891e669226d209dd3
kiraGit67 <kirsten@sais.de>
2023-12-17 18:19:13 +0100

    adds new AppLink :to={name: 'contact'}

--- src/components/FooterNavigation.vue
+++ src/components/FooterNavigation.vue
@@ -3,5 +3,6 @@
     <AppLink :to="{ name: 'about' }">About Us</AppLink>
     <AppLink :to="{ name: 'imprint' }">Imprint</AppLink>
     <AppLink :to="{ name: 'terms' }">Terms &amp; Conditions</AppLink>
+    <AppLink :to="{ name: 'contact' }">Contact Us</AppLink>
   </div>
 </template>
