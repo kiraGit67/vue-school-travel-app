df65c74ee0bc59c86cdf1c2fd630306dfae10dc5
kiraGit67 <kirsten@sais.de>
2023-12-17 16:52:18 +0100

    adds src/components/DestinationCard.vue

---
+++ src/components/DestinationCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <article class="destination-card">
+    <img
+      :src="`/images/${destination.image}`"
+      :alt="destination.name"
+      :title="destination.name"
+    />
+    <div>
+      <h2>{{ destination.name }}</h2>
+      <p>{{ destination.description }}</p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    destination: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.destination-card {
+  border-radius: 0.75rem;
+  border: 1px solid turquoise;
+  background-color: white;
+}
+
+.destination-card > img {
+  width: 100%;
+  border-top-right-radius: 0.75rem;
+  border-top-left-radius: 0.75rem;
+}
+
+.destination-card > div,
+.destination-card > div > h2 {
+  padding-inline: 1.5rem;
+  text-align: center;
+}
+
+.destination-card > div > h2 {
+  color: turquoise;
+}
+</style>
