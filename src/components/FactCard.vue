c991a24b682595b0dffb27d0cd2a881f2ff6aabb
kiraGit67 <kirsten@sais.de>
2023-12-17 14:42:08 +0100

    adds src/components/FactCard.vue

---
+++ src/components/FactCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <article class="fact">
+    <img
+      :src="factImageSource"
+      :alt="factCount + ' ' + factTitle"
+      :title="fact.count + ' ' + fact.title"
+    />
+    <div class="text">
+      <h2>{{ factCount + " " + factTitle }}</h2>
+      <p>{{ factDescription }}</p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    factImageSource: {
+      type: String,
+      required: true,
+    },
+    factID: {
+      type: Number,
+      required: true,
+    },
+    factCount: {
+      type: Number,
+      required: false,
+    },
+    factTitle: {
+      type: String,
+      required: true,
+    },
+    factDescription: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fact {
+  display: flex;
+  gap: 1rem;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0.5rem;
+  background-color: white;
+  border: 1px solid #686dfa;
+  border-radius: 0.5rem;
+}
+
+.fact h2 {
+  color: #686dfa;
+}
+</style>
