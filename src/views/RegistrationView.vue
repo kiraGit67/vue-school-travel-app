cc391e72274445c7be3730b822dcd3fa621d09d5
kiraGit67 <kirsten@sais.de>
2023-12-21 12:48:11 +0100

    adds src/views/RegistrationView.vue

---
+++ src/views/RegistrationView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Registration</h1>
+    <p>Your Data has been registered successfully.</p>
+    <p><span>Firstname:</span><span></span></p>
+    <p><span>Lastname:</span><span></span></p>
+    <p><span>Email:</span><span></span></p>
+    <p><span>Company Name:</span><span></span></p>
+    <p><span>Chosen Destination:</span><span></span></p>
+    <p>Additional Notes: <br /></p>
+  </div>
+</template>
